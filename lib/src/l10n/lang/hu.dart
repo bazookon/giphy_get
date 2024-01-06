f0abc80ffa292c21df01ee2548a7efe07e98db8d
Szilard <szilard@szurti.hu>
2024-01-06 18:05:27 +0100

    Create hu.dart

---
+++ lib/src/l10n/lang/hu.dart
@@ -0,0 +1,34 @@
+import '../default_localizations.dart';
+
+class HuLocalizations extends GiphyGetUILocalizationLabels {
+  @override
+  final String searchInputLabel;
+
+  @override
+  final String emojisLabel;
+
+  @override
+  final String gifsLabel;
+
+  @override
+  final String stickersLabel;
+
+  @override
+  final String moreBy;
+
+  @override
+  final String viewOnGiphy;
+
+  @override
+  final String poweredByGiphy;
+
+  const EnLocalizations({
+    this.searchInputLabel = 'GIPHY keresés',
+    this.emojisLabel = 'Emojik',
+    this.gifsLabel = 'GIFek',
+    this.stickersLabel = 'Matricák',
+    this.moreBy = 'Több általa:',
+    this.viewOnGiphy = 'Megtekintés GIPHY-n',
+    this.poweredByGiphy = 'Powered by GIPHY',
+  });
+}
